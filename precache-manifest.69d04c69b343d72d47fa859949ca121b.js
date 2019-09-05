d631e94b5088d1758b5dd7263fb1552cef421d06
Emma Judge <emmajudge16@gmail.com>
2019-09-04 23:54:12 -0500

    Updates

---
+++ precache-manifest.69d04c69b343d72d47fa859949ca121b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6297202e81e6bec606f85d735168d45c",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "2f8a118c4980c2e2d0da",
+    "url": "/clickyGame/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9c26d44c926e8bb0d0d9",
+    "url": "/clickyGame/static/js/2.d8983aff.chunk.js"
+  },
+  {
+    "revision": "2f8a118c4980c2e2d0da",
+    "url": "/clickyGame/static/js/main.cb46379c.chunk.js"
+  },
+  {
+    "revision": "d3f02d10d87c7ea9b8fa",
+    "url": "/clickyGame/static/js/runtime~main.79f9c7b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickyGame/static/media/logo.5d5d9eef.svg"
+  }
+]);
